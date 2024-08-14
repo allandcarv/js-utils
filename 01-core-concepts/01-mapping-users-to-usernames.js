b9f7818542c852e579a1cd5917498f9aac830bdc
Allan Carvalho <allan.carvalho@dynatrace.com>
2024-08-14 20:54:04 +0200

    feat: core concepts - mapping users to usernames

---
+++ 01-core-concepts/01-mapping-users-to-usernames.js
@@ -0,0 +1,39 @@
+/**
+ * 1. Get array of names from given array of users
+ * 2. Get back only active users.
+ * 3. Sort users by age descending
+ */
+
+const users = [
+  {
+    id: 1,
+    name: 'Jack',
+    isActive: true,
+    age: 20,
+  },
+  {
+    id: 2,
+    name: 'John',
+    isActive: true,
+    age: 18,
+  },
+  {
+    id: 3,
+    name: 'Mike',
+    isActive: false,
+    age: 30,
+  },
+];
+
+const getUserName = (users) => users.map((user) => user.name);
+
+const getActiveUsers = (users) => users.filter((user) => user.isActive);
+
+const sortUsersByAge = (users, order = 'asc') =>
+  users.toSorted((userA, userB) =>
+    order === 'asc' ? userA.age - userB.age : userB.age - userA.age
+  );
+
+console.log(getUserName(users));
+console.log(getActiveUsers(users));
+console.log(sortUsersByAge(users, 'desc'));
